4c6802b2cb29940dc6bd748501584ce1e14f7b7b
vamshi <svamshikrish02@gmail.com>
2023-08-23 09:37:36 +0530

    changes for onboarding and login

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -40,74 +40,9 @@
 //   )
 // }
 
-// import { currentUser } from "@clerk/nextjs";
-// import { redirect } from "next/navigation";
-
-// import ThreadCard from "@/components/cards/ThreadCard";
-// import Pagination from "@/components/shared/Pagination";
-
-// import { fetchPosts } from "@/lib/actions/thread.actions";
-// import { fetchUser } from "@/lib/actions/user.actions";
-
-// async function Home({
-//   searchParams,
-// }: {
-//   searchParams: { [key: string]: string | undefined };
-// }) {
-//   const user = await currentUser();
-//   if (!user) return null;
-
-//   const userInfo = await fetchUser(user.id);
-//   if (!userInfo?.onboarded) redirect("/onboarding");
-
-//   const result = await fetchPosts(
-//     searchParams.page ? +searchParams.page : 1,
-//     30
-//   );
-
-//   return (
-//     <>
-//       <h1 className='head-text text-left'>Home</h1>
-
-//       <section className='mt-9 flex flex-col gap-10'>
-//         {result.posts.length === 0 ? (
-//           <p className='no-result'>No threads found</p>
-//         ) : (
-//           <>
-//             {result.posts.map((post) => (
-//               <ThreadCard
-//                 key={post._id}
-//                 id={post._id}
-//                 currentUserId={user.id}
-//                 parentId={post.parentId}
-//                 content={post.text}
-//                 author={post.author}
-//                 community={post.community}
-//                 createdAt={post.createdAt}
-//                 comments={post.children}
-//               />
-//             ))}
-//           </>
-//         )}
-//       </section>
-
-//       <Pagination
-//         path='/'
-//         pageNumber={searchParams?.page ? +searchParams.page : 1}
-//         isNext={result.isNext}
-//       />
-//     </>
-//   );
-// }
-
-// export default Home;
-
-
-
-
 import Pagination from "@/components/shared/Pagination";
 import { redirect } from "next/navigation";
-import { SignIn, currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import ThreadCard from "@/components/cards/ThreadCard";
@@ -129,6 +64,9 @@ async function Home({
 
   </div>);
 
+  if (!user) return null;
+
+
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
@@ -136,6 +74,7 @@ async function Home({
     searchParams.page ? +searchParams.page : 1,
     30
   );
+
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
@@ -170,4 +109,75 @@ async function Home({
     </>
   );
 }
+
 export default Home;
+
+
+
+
+// import Pagination from "@/components/shared/Pagination";
+// import { redirect } from "next/navigation";
+// import { currentUser } from "@clerk/nextjs";
+// import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+// import ThreadCard from "@/components/cards/ThreadCard";
+// import { fetchPosts } from "@/lib/actions/thread.actions";
+// import { fetchUser } from "@/lib/actions/user.actions";
+
+
+// async function Home({
+//   searchParams,
+// }: {
+//   searchParams: { [key: string]: string | undefined };
+// }) {
+//   const user = await currentUser();
+//   if (!user) return (<div className="text-center ">
+//     <p className="text-white text-8xl my-8">Please Login to continue</p>
+//     <Button asChild>
+//   <Link href="/sign-in">Login</Link>
+// </Button>
+
+//   </div>);
+
+//   const userInfo = await fetchUser(user.id);
+//   if (!userInfo?.onboarded) redirect("/onboarding");
+
+//   const result = await fetchPosts(
+//     searchParams.page ? +searchParams.page : 1,
+//     30
+//   );
+//   return (
+//     <>
+//       <h1 className='head-text text-left'>Home</h1>
+
+//       <section className='mt-9 flex flex-col gap-10'>
+//         {result.posts.length === 0 ? (
+//           <p className='no-result'>No threads found</p>
+//         ) : (
+//           <>
+//             {result.posts.map((post) => (
+//               <ThreadCard
+//                 key={post._id}
+//                 id={post._id}
+//                 currentUserId={user.id}
+//                 parentId={post.parentId}
+//                 content={post.text}
+//                 author={post.author}
+//                 community={post.community}
+//                 createdAt={post.createdAt}
+//                 comments={post.children}
+//               />
+//             ))}
+//           </>
+//         )}
+//       </section>
+
+//       <Pagination
+//         path='/'
+//         pageNumber={searchParams?.page ? +searchParams.page : 1}
+//         isNext={result.isNext}
+//       />
+//     </>
+//   );
+// }
+// export default Home;
